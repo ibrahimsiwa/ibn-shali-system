59b781716231d88f227206316f600a24af38c5b2
ibrahimsiwa <siwaoasis360@gmail.com>
2026-07-18 06:27:56 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
+    return (
     <html lang="ar" dir="rtl">
       <body className={inter.className}>{children}</body>
     </html>
