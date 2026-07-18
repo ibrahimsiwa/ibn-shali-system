5cb1f434a0634476f3d4255b9ef1544444b39ae9
ibrahimsiwa <siwaoasis360@gmail.com>
2026-07-18 06:24:20 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'نظام ابن شالي',
+ title: 'نظام ابن شالي',
   description: 'منظومة إدارة الكاشير والمبيعات',
   icons: {
     icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌴</text></svg>',
